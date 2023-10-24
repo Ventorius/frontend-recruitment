2b8814062cc4ceb66694512ff7464a7cf78ab443
Sebastian Basznianin <avaven@gmail.com>
2023-10-24 14:30:37 +0200

    feat: add iframe height adjusting

--- src/02-widget/IframeApp.tsx
+++ src/02-widget/IframeApp.tsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const IframeApp = () => {
+  const onResize = () => {
+    console.log('resize')
+    window.parent.postMessage(
+      {
+        type: 'resize',
+        //20 is accounting for horizontal bars
+        height: document.body.scrollHeight + 20,
+        width: document.body.scrollWidth,
+      },
+      '*'
+    )
+  }
+
+  const handleParentResize = (event: MessageEvent) => {
+    onResize()
+  }
+
+  useEffect(() => {
+    onResize()
+
+    window.addEventListener('message', handleParentResize)
+
+    return () => {
+      window.removeEventListener('message', handleParentResize)
+    }
+  }, [])
+
   return (
     <div
       style={{
@@ -9,6 +36,7 @@ export const IframeApp = () => {
         padding: '2rem',
         borderRadius: '1rem',
         fontSize: '2rem',
+        height: '100%',
       }}
     >
       Dynamic marketing content will be here
--- src/02-widget/Widget.tsx
+++ src/02-widget/Widget.tsx
@@ -1,15 +1,44 @@
 import React, { useState, useRef, useEffect } from 'react'
 
 import './widget.css'
+import useResizeObserver from './useResizeObserver'
 
 export const Widget = () => {
-  const [width, setWidth] = useState()
-  const iframeContainer = useRef<any>()
+  const [width, setWidth] = useState<number | undefined>()
+  const [height, setHeight] = useState<number | undefined>()
+
+  const iframeContainer = useRef<HTMLIFrameElement | null>(null)
+  const iframe = useRef<HTMLIFrameElement | null>(null)
+
+  const sendMessage = () => {
+    if (iframe.current) {
+      iframe?.current?.contentWindow?.postMessage({
+        type: 'analytics',
+      })
+    }
+  }
 
   useEffect(() => {
-    const measurements = iframeContainer.current.getBoundingClientRect()
+    const measurements = iframeContainer?.current?.getBoundingClientRect()
     if (measurements) {
       setWidth(measurements.width)
+    }
+  }, [])
+  //@ts-ignore
+  useResizeObserver(sendMessage, document.body)
+
+  const handleResize = (event: MessageEvent) => {
+    if (event?.data?.type === 'resize') {
+      setHeight(event.data.height)
+      // setWidth(event.data.width)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('message', handleResize)
+
+    return () => {
+      window.removeEventListener('message', handleResize)
     }
   }, [])
 
@@ -25,7 +54,8 @@ export const Widget = () => {
         ref={iframeContainer}
       >
         <iframe
-          height="117px"
+          ref={iframe}
+          height={height}
           width={width}
           src="/iframe"
           style={{ border: 0 }}
